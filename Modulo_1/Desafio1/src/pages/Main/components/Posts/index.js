3575967392337480eaea65f5d6c586e3c028674e
Diego de Souza Vale Moreno <diego.moreno@linx.com.br>
2019-05-21 11:46:48 -0300

    Testes no emulados de Android e melhoria na exibição da imagem.

--- Modulo_1/Desafio1/src/pages/Main/components/Posts/index.js
+++ Modulo_1/Desafio1/src/pages/Main/components/Posts/index.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Text, Image } from 'react-native';
-import styles from './styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, Image } from "react-native";
+import styles from "./styles";
 
-const Post = ({ data }) => (
-  <View style={styles.container}>
-    <View style={styles.postHeader}>
-      <Text style={styles.title}>{data.title}</Text>
-      <Text style={styles.author}>{data.author}</Text>
+const Post = ({ data }) => {
+  console.log(data);
+  return (
+    <View style={styles.container}>
+      <View style={styles.postHeader}>
+        <Text style={styles.title}>{data.title}</Text>
+        <Text style={styles.author}>{data.author}</Text>
+      </View>
+      <Text style={styles.content}>{data.content}</Text>
+      <Image style={styles.image} source={{ uri: `${data.url}` }} />
     </View>
-    <Text style={styles.content}>{data.content}</Text>
-    <Image style={styles.image} source={{ uri: `${data.url} ` }} />
-  </View>
-);
+  );
+};
 
 Post.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string,
     author: PropTypes.string,
     content: PropTypes.string,
-    url: PropTypes.string,
-  }).isRequired,
+    url: PropTypes.string
+  }).isRequired
 };
 
 export default Post;
--- Modulo_1/Desafio1/src/pages/Main/components/Posts/styles.js
+++ Modulo_1/Desafio1/src/pages/Main/components/Posts/styles.js
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-import { colors } from '~/styles';
+import { StyleSheet } from "react-native";
+import { colors } from "~/styles";
 
 const styles = StyleSheet.create({
   container: {
@@ -7,32 +7,33 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 5,
     marginTop: 20,
-    marginHorizontal: 20,
+    marginHorizontal: 20
   },
 
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: colors.title,
+    fontWeight: "bold",
+    color: colors.title
   },
 
   author: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: colors.author,
-    marginTop: 10,
+    marginTop: 10
   },
 
   content: {
     marginTop: 10,
     fontSize: 16,
-    fontWeight: 'bold',
-    color: colors.post,
+    fontWeight: "bold",
+    color: colors.post
   },
   image: {
-    width: 50,
-    height: 50,
-  },
+    width: 80,
+    height: 80,
+    marginTop: 10
+  }
 });
 
 export default styles;
