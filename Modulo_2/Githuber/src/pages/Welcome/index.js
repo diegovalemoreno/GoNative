94eb7c79678f793ca95c8aa178ca5474b7ee3ffd
Diego de Souza Vale Moreno <diego.moreno@linx.com.br>
2019-05-22 19:40:21 -0300

    Criação do App Githuber, configuração da arquitetura de navergação do APP. Definição de estilos globais.

---
+++ Modulo_2/Githuber/src/pages/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+import styles from "./styles";
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text>Testando</Text>
+  </View>
+);
+export default Welcome;
